ac7b78f111cf6a24867e983b77f66ed20d0531c5
mavagnale <mavagnale@gmail.com>
2024-08-05 15:25:47 +0200

    added wbc in launchfile

--- src/wholeBodyControllerNode.cpp
+++ src/wholeBodyControllerNode.cpp
@@ -2,8 +2,8 @@
 
 int main(int argc, char** argv)
 {
-    ros::init(argc, argv, "main");
+    ros::init(argc, argv, "wholeBodyControllerNode");
     WholeBodyController wbc;
     wbc.run();
-
+    return 0;
 }
